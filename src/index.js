ebf3da9547138116f43251244b93395b3de230a9
Alex H <alex.holder@bigfootds.com>
2024-05-18 21:52:45 +1000

    docs: improve jsdoc typing

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ export default class SuperCamo {
 	 * @param databaseName A string that follows JavaScript variable naming rules. This will be the key needed to access this database from the SuperCamo.activeClients object, and doesn't need to have any commonality with the database directory path.
 	 * @param databaseDirectory A path to a directory to store all NeDB files.
 	 * @param {[{name: string, model: NedbDocument}]} collectionsList An array of objects containing a desired name for a collection as well as the Document-inheriting model that should be used for that collection. You must provide ALL intended models & collections for the database client in this property - don't leave anything out!
-	 * @returns An instance of a NeDB database client.
+	 * @returns {NedbClient} An instance of a NeDB database client.
 	 */
 	static connect = async (databaseName, databaseDirectory = "", collectionsList = []) => {
 		
