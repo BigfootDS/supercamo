82cc5549c6b093677c24318705445a18f16fb34d
Alex <alex.holder@bigfootds.com>
2024-09-22 14:50:14 +1000

    findAndUpdateOneObject tests

--- tests/js/NedbClientUPDATE.test.js
+++ tests/js/NedbClientUPDATE.test.js
@@ -15,6 +15,16 @@ const secondUserData = {
 	bio: {tagline: "Test user, the second.", blurb: "Another super cool test user with a long, descriptive blurb."}
 };
 
+const thirdUserData = {
+	email: "test3@email.com",
+	bio: {tagline: "Test user, the third.", blurb: "Super cool test user with a long, descriptive blurb."}
+};
+
+const fourthUserData = {
+	email: "test4@email.com",
+	bio: {tagline: "Test user, the fourth.", blurb: "Another super cool test user with a long, descriptive blurb."}
+};
+
 const nonexistentUser = {
 	email: "nonexistentemail@email.com",
 	bio: {
@@ -25,6 +35,14 @@ const nonexistentUser = {
 
 const upsertUserOne = {
 	email: "upseruserone@email.com",
+	bio: {
+		tagline: "Made via upsert.",
+		blurb: "Upsert is pretty cool."
+	}
+}
+
+const upsertUserTwo = {
+	email: "upserusertwo@email.com",
 	bio: {
 		tagline: "Made via upsert.",
 		blurb: "Upsert is pretty cool."
@@ -62,6 +80,10 @@ beforeAll(async () => {
 	await newClient.insertOne("Users", firstUserData);
 
 	await newClient.insertOne("Users", secondUserData);
+
+	await newClient.insertOne("Users", thirdUserData);
+
+	await newClient.insertOne("Users", fourthUserData);
 })
 
 describe("Database can perform UPDATE operations", () => {
@@ -96,7 +118,32 @@ describe("Database can perform UPDATE operations", () => {
 
 
 	describe("findAndUpdateOneObject operations", () => {
+		test("returns null when no data matches the given query.", async () => {
+			let result = await newClient.findAndUpdateOneObject("Users",{email: nonexistentUser.email}, {email: "valueHereDoesntMatter@email.com"});
+			expect(result).toBeNull();
+		});
+		test("returns a document instance when given a valid query and valid update data, specifically for top-level document data.", async () => {
+			let result = await newClient.findAndUpdateOneObject("Users",{email: thirdUserData.email}, {email: "test-modified3@email.com"});
+			expect(result.email).toBe("test-modified3@email.com");
+			expect(result.bio.tagline).toBe(thirdUserData.bio.tagline);
+		});
+		test("returns a document instance when given a valid query and valid update data, specifically for embedded document data.", async () => {
+			let result = await newClient.findAndUpdateOneObject("Users",{email: fourthUserData.email}, {bio: {tagline: "Modified embedded document data."}});
+			expect(result.email).toBe(fourthUserData.email);
+			expect(result.bio.tagline).toBe("Modified embedded document data.");
+		});
+		test("returns a document instance when given a valid query, data, and upsert option, when no document already matches the query.", async () => {
+			let result = await newClient.findAndUpdateOneObject("Users", {email: upsertUserTwo.email}, upsertUserTwo, {upsert: true});
+			expect(result.email).toBe(upsertUserTwo.email);
+			expect(result.bio.tagline).toBe(upsertUserTwo.bio.tagline);
 
+		});
+		test("returns a document creation error when given a valid query and upsert option but invalid data.", async () => {
+			expect(async () => {
+				let result = await newClient.findAndUpdateOneObject("Users", {email: nonexistentUser.email}, invalidUpsertUserOne, {upsert: true});
+				console.log(result.data);
+			}).rejects.toThrowError(ValidationFailureMissingValueForProperty);
+		});
 	});
 
 
